3dfe2652ab5524e8328c745a0c1af954a5ccc18a
intakhabchy <intakhab.chy@gmail.com>
2026-03-09 15:30:42 +0600

    gateway endpoints work

--- api-gateway/routes/api.php
+++ api-gateway/routes/api.php
@@ -12,6 +12,19 @@ Route::get('/users/test', function(){
     return Http::get('http://localhost:8001/api/test')->json();
 });
 
+Route::post('/users/user_registration', function(){
+    $response = Http::post('http://localhost:8001/api/user_registration', request()->all());
+    return response()->json($response->json(), $response->status());
+});
+
+Route::get('/users/user/{id}', function($id){
+    return Http::get('http://localhost:8001/api/user/'.$id)->json();
+});
+
+Route::get('/users/rolelist', function(){
+    return Http::get('http://localhost:8001/api/rolelist')->json();
+});
+
 Route::get('/product/test', function(){
     return Http::get('http://localhost:8002/api/test')->json();
 });
